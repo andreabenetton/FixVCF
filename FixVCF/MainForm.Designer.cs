c3a3484c21860d09775def31c522ff1fb7e0abb7
Andrea Benetton <andrea.benetton@blueteam.ee>
2019-11-19 18:27:03 +0100

    First Commit
    Signed-off-by: Andrea Benetton <andrea.benetton@blueteam.ee>

---
+++ FixVCF/MainForm.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace FixVCF
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialogLoadSelector = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxLoadFullPath = new System.Windows.Forms.TextBox();
+            this.buttonLoadSelectorOpen = new System.Windows.Forms.Button();
+            this.buttonProcess = new System.Windows.Forms.Button();
+            this.textBoxSaveFullPath = new System.Windows.Forms.TextBox();
+            this.saveFileDialogSelector = new System.Windows.Forms.SaveFileDialog();
+            this.progressBarProcess = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // openFileDialogLoadSelector
+            // 
+            this.openFileDialogLoadSelector.FileName = "openFileDialog1";
+            this.openFileDialogLoadSelector.Filter = "VCF files|*.vcf";
+            // 
+            // textBoxLoadFullPath
+            // 
+            this.textBoxLoadFullPath.Location = new System.Drawing.Point(12, 12);
+            this.textBoxLoadFullPath.Name = "textBoxLoadFullPath";
+            this.textBoxLoadFullPath.Size = new System.Drawing.Size(201, 20);
+            this.textBoxLoadFullPath.TabIndex = 0;
+            this.textBoxLoadFullPath.DoubleClick += new System.EventHandler(this.TextBoxLoadFullPath_DoubleClick);
+            // 
+            // buttonLoadSelectorOpen
+            // 
+            this.buttonLoadSelectorOpen.Enabled = false;
+            this.buttonLoadSelectorOpen.Location = new System.Drawing.Point(219, 10);
+            this.buttonLoadSelectorOpen.Name = "buttonLoadSelectorOpen";
+            this.buttonLoadSelectorOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoadSelectorOpen.TabIndex = 1;
+            this.buttonLoadSelectorOpen.Text = "Look For";
+            this.buttonLoadSelectorOpen.UseVisualStyleBackColor = true;
+            this.buttonLoadSelectorOpen.Click += new System.EventHandler(this.ButtonLoadSelectorOpen_Click);
+            // 
+            // buttonProcess
+            // 
+            this.buttonProcess.Enabled = false;
+            this.buttonProcess.Location = new System.Drawing.Point(219, 53);
+            this.buttonProcess.Name = "buttonProcess";
+            this.buttonProcess.Size = new System.Drawing.Size(75, 23);
+            this.buttonProcess.TabIndex = 2;
+            this.buttonProcess.Text = "Process";
+            this.buttonProcess.UseVisualStyleBackColor = true;
+            this.buttonProcess.Click += new System.EventHandler(this.ButtonProcess_Click);
+            // 
+            // textBoxSaveFullPath
+            // 
+            this.textBoxSaveFullPath.Location = new System.Drawing.Point(12, 53);
+            this.textBoxSaveFullPath.Name = "textBoxSaveFullPath";
+            this.textBoxSaveFullPath.Size = new System.Drawing.Size(201, 20);
+            this.textBoxSaveFullPath.TabIndex = 3;
+            this.textBoxSaveFullPath.DoubleClick += new System.EventHandler(this.TextBoxSaveFullPath_DoubleClick);
+            // 
+            // saveFileDialogSelector
+            // 
+            this.saveFileDialogSelector.DefaultExt = "vcf";
+            this.saveFileDialogSelector.Filter = "VCF files|*.vcf";
+            // 
+            // progressBarProcess
+            // 
+            this.progressBarProcess.Location = new System.Drawing.Point(13, 95);
+            this.progressBarProcess.Name = "progressBarProcess";
+            this.progressBarProcess.Size = new System.Drawing.Size(200, 23);
+            this.progressBarProcess.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 161);
+            this.Controls.Add(this.progressBarProcess);
+            this.Controls.Add(this.textBoxSaveFullPath);
+            this.Controls.Add(this.buttonProcess);
+            this.Controls.Add(this.buttonLoadSelectorOpen);
+            this.Controls.Add(this.textBoxLoadFullPath);
+            this.Name = "MainForm";
+            this.Text = "ProtonMail to LineageOS vcf fixer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialogLoadSelector;
+        private System.Windows.Forms.TextBox textBoxLoadFullPath;
+        private System.Windows.Forms.Button buttonLoadSelectorOpen;
+        private System.Windows.Forms.Button buttonProcess;
+        private System.Windows.Forms.TextBox textBoxSaveFullPath;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogSelector;
+        private System.Windows.Forms.ProgressBar progressBarProcess;
+    }
+}
+
---
+++ FixVCF/MainForm.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using MixERP.Net.VCards;
+using System.Linq;
+
+namespace FixVCF
+{
+    public partial class MainForm : Form
+    {
+        private IEnumerable<VCard> vcards;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonLoadSelectorOpen_Click(object sender, EventArgs e)
+        {
+            
+            string path = textBoxLoadFullPath.Text;
+            try
+            {
+                string contents = File.ReadAllText(path, Encoding.UTF8);
+                vcards = Deserializer.GetVCards(contents);
+            }
+            catch (IOException)
+            {
+            }
+        }
+
+        private void ButtonProcess_Click(object sender, EventArgs e)
+        {
+            List<VCard> vout = new List<VCard>();
+            double n = 100.0/vcards.Count();
+            int count = 0;
+
+            foreach (var vcard in vcards)
+            {
+                count += 1;
+                string[] words = vcard.FormattedName.Split(' ');
+                vcard.FirstName = words[0];
+
+                if (words.Length == 2) vcard.LastName = words[1];
+                if (words.Length == 3) vcard.LastName = words[1] + " " + words[2];
+
+                vcard.CustomExtensions = null;
+                vcard.UniqueIdentifier = null;
+
+                if (vcard.Telephones != null)
+                {
+                    foreach (var tel in vcard.Telephones)
+                    {
+                        tel.Number = TrimAllWithInplaceCharArray(tel.Number);
+                        tel.Type = MixERP.Net.VCards.Types.TelephoneType.Voice;
+                    }
+                    vout.Add(vcard);
+                }
+                progressBarProcess.Value = Convert.ToInt32(count * n);
+            }
+
+            string content = MixERP.Net.VCards.Serializer.VCardCollectionSerializer.Serialize(vout);
+            File.WriteAllText(textBoxSaveFullPath.Text, content);
+        }
+
+        private void TextBoxLoadFullPath_DoubleClick(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialogLoadSelector.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                textBoxLoadFullPath.Text = openFileDialogLoadSelector.FileName;
+                buttonLoadSelectorOpen.Enabled = true;
+            }
+        }
+
+        private void TextBoxSaveFullPath_DoubleClick(object sender, EventArgs e)
+        {
+            if (saveFileDialogSelector.ShowDialog() == DialogResult.OK)
+            {
+                textBoxSaveFullPath.Text = saveFileDialogSelector.FileName;
+                buttonProcess.Enabled = true;
+            }
+        }
+
+        public static string TrimAllWithInplaceCharArray(string str)
+        {
+            var len = str.Length;
+            var src = str.ToCharArray();
+            int dstIdx = 0;
+            for (int i = 0; i < len; i++)
+            {
+                var ch = src[i];
+                if (!isWhiteSpace(ch))
+                    src[dstIdx++] = ch;
+            }
+            return new string(src, 0, dstIdx);
+        }
+
+        public static bool isWhiteSpace(char ch)
+        {
+            // this is surprisingly faster than the equivalent if statement
+            switch (ch)
+            {
+                case '\u0009':
+                case '\u000A':
+                case '\u000B':
+                case '\u000C':
+                case '\u000D':
+                case '\u0020':
+                case '\u0085':
+                case '\u00A0':
+                case '\u1680':
+                case '\u2000':
+                case '\u2001':
+                case '\u2002':
+                case '\u2003':
+                case '\u2004':
+                case '\u2005':
+                case '\u2006':
+                case '\u2007':
+                case '\u2008':
+                case '\u2009':
+                case '\u200A':
+                case '\u2028':
+                case '\u2029':
+                case '\u202F':
+                case '\u205F':
+                case '\u3000':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
